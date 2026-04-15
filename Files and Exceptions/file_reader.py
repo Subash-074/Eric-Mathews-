a43a0a3e344bd9699e6ff6bfe94c005bcf2c3f57
Subash <sapkotasubash784@gmail.com>
2026-04-15 20:54:58 +0200

    enjoy reading
    good good go on

---
+++ Files and Exceptions/file_reader.py
@@ -0,0 +1,5 @@
+with open(r"C:\Users\subas\Documents\GitHub\Eric-Mathews-\Files and Exceptions\pi_digits.txt") as file_object:
+      contents = file_object.read()
+      print(contents)
+     
+ 
---
+++ Files and Exceptions/writingtoafile.py
@@ -0,0 +1,9 @@
+#Writing to a file 
+
+
+filename=r'C:\Users\subas\Documents\GitHub\Eric-Mathews-\Files and Exceptions\programmint.txt'
+
+with open(filename, 'w') as file_object:
+      file_object.write('I love programming. \n')
+      file_object.write('I love creating new games.\n ')
+      file_object.write('I love creating new games.\n ')
